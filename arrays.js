51c445f9e2106711c2760520fd7f79bd5ae5f204
David Majholt <d.boy.dragoz@gmail.com>
2025-08-25 10:04:18 +0200

    Alle Opgaver Done
    Tilføj konsollogfiler for opgavestatus.

--- arrays.js
+++ arrays.js
@@ -5,6 +5,7 @@ let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
 // --- Løs opgaverne herunder ---
 //opgave 1: Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle de tal som er større end 20.
+console.log("----------- OPGAVE 1 -----------")
 /** * * @param {number} amount "0-9999"  * @returns "LIST"*/
 function returnNumberAbove(amount) { return numbersArray.filter((cValue) => cValue > amount); }
 console.log(returnNumberAbove(20))
@@ -13,6 +14,7 @@ console.log(returnNumberAbove(20))
 // Tilføj katten "Sniffles" til catsArray.
 // Udskriv arrayet i konsollen.
 // console.log("Opgave 2", ...)
+console.log("----------- OPGAVE 2 -----------")
 /** ** @param {string} name "fx: 'Sniffles'"  */
 function addCatName(name) { catsArray.push(name); console.log(`Added new cat named: ${name}`); console.log(catsArray); }
 addCatName("Sniffles")
@@ -21,6 +23,7 @@ addCatName("Sniffles")
 // Check om der i dogsArray er en hund som hedder "Trixie".
 // Udskriv "true" eller "false" i konsollen afhængig af svaret.
 // console.log("Opgave 3a", ...)
+console.log("----------- OPGAVE 3a ----------")
 /** ** @param {string} name "fx: 'Trixie'" */
 function returnBoolOfName(name) { let returning; dogsArray.forEach((cValue) => { if (cValue == name) { return true } else { returning = false } }); return returning }
 console.log(`Found the dog: ${returnBoolOfName("Trixie")}`)
@@ -29,12 +32,14 @@ console.log(`Found the dog: ${returnBoolOfName("Trixie")}`)
 // Check om der i dogsArray er en hund som hedder "Baxter".
 // Udskriv "true" eller "false" i konsollen afhængig af svaret.
 // console.log("Opgave 3b", ...)
+console.log("----------- OPGAVE 3b ----------")
 console.log(`Found the dog: ${returnBoolOfName("Baxter")}`)
 
 //     Opgave 4a
 // Check om der i catsArray er en kat som hedder "Bagheera".
 // Udskriv katten i konsollen hvis den findes og ellers "undefined".
 // console.log("Opgave 4a", ...)
+console.log("----------- OPGAVE 4a ----------")
 /** ** @param {string} name "fx: 'Bagheera'" */
 function returnCatBoolName(name) { dogsArray.forEach((cValue) => { if (cValue == name) { return name } }); return undefined }
 console.log(`Found the cat: ${returnBoolOfName("Bagheera")}`)
@@ -43,12 +48,14 @@ console.log(`Found the cat: ${returnBoolOfName("Bagheera")}`)
 // Check om der i catsArray er en kat som hedder "Salem".
 // Udskriv katten i konsollen hvis den findes og ellers "undefined".
 // console.log("Opgave 4b", ...)
+console.log("----------- OPGAVE 4b ----------")
 console.log(`Found the cat: ${returnBoolOfName("Salem")}`)
 
 //     Opgave 5
 // Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
 // Udskriv det nye array i konsollen:
 // console.log("Opgave 5", ...)
+console.log("----------- OPGAVE 5 -----------")
 /** ** @param {number} number "fx: '3'" */
 function returnArrayWithExtra(number) { let returnArray = []; numbersArray.forEach((cValue) => { returnArray.push(cValue * number) }); return returnArray }
 console.log(returnArrayWithExtra(3))
@@ -58,11 +65,13 @@ console.log(returnArrayWithExtra(3))
 // (Sådan her: BarneyMollyBaxter...)
 // Udskriv strengen konsollen:
 // console.log("Opgave 6", ...)
+console.log("----------- OPGAVE 6 -----------")
 console.log(dogsArray.toString().replaceAll(",", ""))
 
 //     Opgave 7a
 // Find ud af hvilket index "Mango" har i fruitsArray.
 // Udskriv indexet i konsollen.
+console.log("----------- OPGAVE 7a ----------")
 /** ** @param {string} name "fx: 'Mango'" */
 function findReturnFruitNamed(name) { return fruitsArray.indexOf(name) }
 console.log(findReturnFruitNamed("Mango"))
@@ -70,6 +79,7 @@ console.log(findReturnFruitNamed("Mango"))
 // Opgave 7b
 // Find ud af hvilket index "Blåbær" har i fruitsArray.
 // Udskriv indexet i konsollen.
+console.log("----------- OPGAVE 7b ----------")
 console.log(findReturnFruitNamed("Blåbær"))
 
 // Opgave 8
@@ -77,6 +87,7 @@ console.log(findReturnFruitNamed("Blåbær"))
 // Den familie der har hunden "Polly" er desværre blevet forhindret i at deltage i udstillingen.
 // Fjern hunden "Polly" fra dogsArray og udskriv det nye array i konsollen
 //  ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
+console.log("----------- OPGAVE 8 -----------")
 /** ** @param {string} name "fx: 'Polly'" */
 function removeDogFromArray(name) { dogsArray.splice(dogsArray.indexOf(name), dogsArray.indexOf(name)); return name }
 console.log(`Removed Dog named: ${removeDogFromArray("Polly")}`)
